0d5a5b68866ee188d878f99b22915c72a9a61656
dar4kamal <sasaa9646@gmail.com>
2019-05-12 01:16:17 +0200

    modified  product Page

--- client/vidly/src/components/common/singleBook.jsx
+++ client/vidly/src/components/common/singleBook.jsx
@@ -1,43 +1,104 @@
 import React, { Component } from "react";
+import NavBar from "./navBar";
+import axios from "axios";
+
 class SingleBook extends Component {
-  state = { product: this.props.location.state.data };
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: this.props.location.state.data,
+      reviews: [],
+      msg: ""
+    };
+  }
+
+  async componentDidMount() {
+    const { product } = this.state;
+    const res = await axios.get(
+      "http://localhost:5000/api/reviews/products/" + product._id
+    );
+
+    if (res.data.message) {
+      this.setState({ msg: res.data.message });
+    } else {
+      console.log("reviews", res.data.data);
+      this.setState({ reviews: res.data.data });
+    }
+    this.setState({ product: this.props.location.state.data });
+  }
 
   render() {
-    const { product } = this.state;
-    console.log(product);
+    const { product, reviews, msg } = this.state;
 
     return (
-      <div className="card mb-3">
-        <div
-          className="row no-gutters"
-          style={{ width: "100%", height: "100%" }}
-        >
-          <div className="col-md-4">
-            <img
-              src={product.image}
-              className="card-img"
-              alt="..."
-              style={{ width: "40%", marginLeft: 150, marginTop: 100 }}
-            />
-          </div>
-          <div className="col-md-8">
-            <div
-              className="card-body"
-              style={{ marginLeft: 10, marginTop: 100 }}
-            >
-              <h2 className="card-title">{product.title}</h2>
-              <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="card-text">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </p>
+      <React.Fragment>
+        <NavBar />
+        <div className="card mb-3">
+          <div
+            className="row no-gutters"
+            style={{ width: "100%", height: "100%" }}
+          >
+            <div className="col-md-4">
+              <img
+                src={product.image}
+                className="card-img"
+                alt="..."
+                style={{ width: "40%", marginLeft: 150, marginTop: 100 }}
+              />
+            </div>
+            <div className="col-md-8">
+              <div
+                className="card-body"
+                style={{ marginLeft: 10, marginTop: 100 }}
+              >
+                <h1 className="card-title">{product.title}</h1>
+                <h3 className="card-text">{product.authors}</h3>
+                <br />
+                <h5>Book Details</h5>
+                <p className="card-text">
+                  In Stock{" "}
+                  <small className="text-muted">{product.in_stock}</small>
+                  <br />
+                  Lang <small className="text-muted">{product.lang}</small>
+                  <br />
+                  Price <small className="text-muted">{product.price}</small>
+                  <br />
+                  Isbn <small className="text-muted">{product.isbn}</small>
+                  <br />
+                  Publication_year{" "}
+                  <small className="text-muted">
+                    {product.publication_year}
+                  </small>
+                  <br />
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        <div style={{ textAlign: "center" }}>
+          <h2>User Reviews</h2>
+          {!msg ? (
+            <div className="border container">
+              <div className="row">
+                {reviews.map((item, index) => {
+                  return (
+                    <div className="col-md-12" key={index}>
+                      <div className="card card-inverse card-info">
+                        <div className="card-block">
+                          <div className="card-text">{item.username}</div>
+                          <div className="card-text">{item.review}</div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          ) : (
+            <h1>{msg}</h1>
+          )}
+        </div>
+      </React.Fragment>
     );
   }
 }
